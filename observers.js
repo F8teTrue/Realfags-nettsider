7e2016726c68163f6dc3e17d427e5de04a238834
F8teTrue <preben.dahl2006@gmail.com>
2023-09-04 13:03:59 +0200

    Fade-in

--- observers.js
+++ observers.js
@@ -1,19 +1,22 @@
 const faders = document.querySelectorAll('.fade-in')
 
-const appearOptions =  {}; 
+const appearOptions =  {
+    threshold: 0.5
+}; 
 
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-entries.forEach(entry => {
-if (!entry.isIntersecting) {
-    return;
-} else {
-    entry.target.classList.add('appear');
-    appearOnScroll.unobserve(entry.target);
-}
-})
-
+    console.log(entries)
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {  
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
 }, appearOptions);
+
 
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
-})
+});
